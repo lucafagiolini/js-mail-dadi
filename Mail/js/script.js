c63c9fd9431f3a82db3c5d3c258b661682578891
Luca <lucafagiolini@live.it>
2024-02-04 01:18:55 +0100

    email exercise completed

--- Mail/js/script.js
+++ Mail/js/script.js
@@ -1,14 +1,36 @@
 
+const emailList = ["francorossi@gmail.com", "mariablu@gmail.com", "alessiagiallo@gmail.com"];
+console.log(emailList);
+
+// 
+document.querySelector("#submit-btn").addEventListener("click", userClearance);
+function userClearance() {
+
+    // raccolgo l'email inserita dall'utente
+    let userEmail = document.querySelector("#email-input").value;
+    console.log("user-email " + userEmail);
+
+    // pusho l'email inserita dall'utente nella lista emailList
+    emailList.push(userEmail);
+    console.log(emailList);
+
+    // lista email autorizzate
+
+    // controllo se l'email inserita dall'utente è presente nella lista emailList
+    for (let i = 0; i < emailList.length; i++) {
+        if (userEmail === emailList[i]) {
+            console.log("email autorizzata");
+            // document.querySelector("#email-input").value = "";
+            // document.querySelector("#email-input").style.border = "1px solid green";
+            // document.querySelector("#email-input").style.color = "green";
+            // document.querySelector("#email-input").placeholder = "email autorizzata";
+            document.querySelector("#result-box").classList.remove("d-none")
+            document.querySelector("#form").classList.add("d-none")
+            return;
+
+        }
+    }
+}
 
 
 
-
-
-
-
-
-
-
-
-
-document.querySelector("#submit-btn").addEventListener("click", function () {
